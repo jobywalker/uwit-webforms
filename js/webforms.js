ed0e308b5a3d73e1b9455c2bf5bf50f4099e7116
jodytate <jtate@uw.edu>
2012-11-28 13:08:52 -0800

    test form
    first successful build of a form from json

--- js/webforms.js
+++ js/webforms.js
@@ -25,6 +25,53 @@
 }());
 
 (function() { 
+    
     'use strict';
+
+    var ajaxConsoleLog = function (textStatus, jqXHR) {
+        var face;
+        if (textStatus === 'success') {
+            face = ':)';
+        } else {
+            face = ':(';
+        }
+        console.log(face + ' ' +  textStatus.toUpperCase()  + ' -- jqXHR.statusText = ' + jqXHR.statusText + ', textStatus = ' + textStatus);
+    };
+
+    var tsmForm = {};
+
+    tsmForm.displayForm = function () {
+        console.log('running getFormJson');
+        $.ajax({
+            url: 'tsm-form-fields.json',
+            dataType: 'json',
+            contentType: 'application/json',
+            success: function (data, textStatus, jqXHR) {
+                ajaxConsoleLog(textStatus, jqXHR);
+                tsmForm.buildForm(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                ajaxConsoleLog(textStatus, jqXHR);
+            }
+        });
+    };
+
+    tsmForm.buildForm = function (json) {
+        var html = '';
+        $.each(json, function(k, v){
+            console.log('k = ' + k + ', v = ' + v);
+            html += '<div class="control-group">' +
+                '<label class="control-label" for="input' + v.id + '">' + v.name + '</label>' +
+                '<div class="controls">' + 
+                '<input type="' + v.inputType +'" id="inputEmail" placeholder="' + v.placeholder + '">' + 
+                '</div>' +
+                '</div>';
+        });
+        $('.form-horizontal').append(html);
+    };
+
+    $(function () { 
+        tsmForm.displayForm();
+    });
 
 }());
