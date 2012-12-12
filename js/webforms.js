cb979465e5343e5b3f55796bbb8c8a65b644c23b
jodytate <jtate@uw.edu>
2012-12-11 17:16:54 -0800

    moved to non-test file, test config file
    the form is now built using a config file--pretty nice--, i changed the
    file name to just "test" for now

--- js/webforms.js
+++ js/webforms.js
@@ -1,77 +1,193 @@
-/* global $ */
+var webForms = {};
 
-// https://github.com/h5bp/html5-boilerplate/blob/master/js/plugins.js
-// Avoid `console` errors in browsers that lack a console.
-(function() {
-    var method;
-    var noop = function noop() {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+webForms.testMe = function (arg) {
+    'use strict';
+    console.log('TESTING with testMe : ' + arg);
+};
 
-    while (length--) {
-        method = methods[length];
-
-        // Only stub undefined methods.
-        if (!console[method]) {
-            console[method] = noop;
+webForms.getQueue = function (id) { 
+    'use strict';
+    console.log('running getQueue')
+    var url = 'queues.json',
+        result = null;
+    $.ajax({
+        url: url,
+        async: false,
+        success: function(data, textStatus, jqXHR) {
+            result = data[id];
+            //$('#uw-netid').text(result.user);
+            // error: jqXHR, textStatus, errorThrown
+            ajaxConsoleLog('getQueue', textStatus, jqXHR);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            ajaxConsoleLog('getQueue', textStatus, jqXHR);
         }
-    }
-}());
+    });
+    console.log('result.queue = ' + result.queue);
+    return result.queue;
+};
 
-(function() { 
-    
+webForms.getNewTicketNumber = function (data) {
+    'use strict';
+    var text = data,
+        textSplit = text.split(' '),
+        wordTicketIndex = textSplit.indexOf('Ticket'),
+        ticketIndex = wordTicketIndex + 1;
+        ticket = textSplit[ticketIndex];
+    return ticket;
+};
+
+// not in use yet:
+//webForms.getTicket = function (number) {
+//    //var url = 'https://rtdev.cac.washington.edu/daw/json/RT/v1/Ticket/Id/' + number;
+//    url = 'text.txt';
+//    $.ajax({
+//        url: url,
+//        success: function (data, textStatus, jqXHR) {
+//            ajaxConsoleLog(textStatus, jqXHR);
+//            console.log(data);
+//            $.each(data, function (key, value) {
+//                console.log('key = ' + key + ', value = ' + value)
+//            });
+//        },
+//        error: function (jqXHR, textStatus, errorThrown) {
+//            ajaxConsoleLog(textStatus, jqXHR);
+//        }    
+//    });
+//};
+
+
+webForms.parcaeUser = function (user) {
+    user = webForms.getUser();
+    // https://shades.cac.washington.edu/parcae/api/user/director/uwnetid/joby/
+    https://shades.cac.washington.edu/parcae/api/user/director/app/RT/uwnetid/joby/
+    'use strict';
+    var url = 'https://shades.cac.washington.edu/parcae/api/user/director/app/RT/uwnetid/jtate/';
+    $.ajax({
+        url: url,
+        success: function (data, textStatus, jqXHR) {
+            ajaxConsoleLog('parcaeUser', textStatus, jqXHR);
+            console.log(data);
+            //$.each(data, function (key, value) {
+            //    console.log('key = ' + key + ', value = ' + value)
+            //});
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            ajaxConsoleLog('parcaeUser', textStatus, jqXHR);
+        }    
+    });
+};
+
+// https://rtdev.cac.washington.edu/webforms/js/rt-test.js
+webForms.createTicket = function(subject, message) {
     'use strict';
 
-    var ajaxConsoleLog = function (textStatus, jqXHR) {
-        var face;
-        if (textStatus === 'success') {
-            face = ':)';
-        } else {
-            face = ':(';
-        }
-        console.log(face + ' ' +  textStatus.toUpperCase()  + ' -- jqXHR.statusText = ' + jqXHR.statusText + ', textStatus = ' + textStatus);
-    };
+    var queue = 'SSG::Test',
+        rtDevURL = 'https://rtdev.cac.washington.edu/Ticket/Display.html?id=',
+        ticket,
+        ticketStringPost = {
+            UpdateTickets : 'Upload',
+            resultsonly : "on",
+            nodecoration : "on",
+            string : '===Create-Ticket: ticket1\nQueue: ' + queue + '\nSubject: ' + subject + ', ' + theTimeNow() + '\nContent: ' + message + '\nENDOFCONTENT'
+        };
 
-    var tsmForm = {};
-
-    tsmForm.displayForm = function () {
-        console.log('running getFormJson');
-        $.ajax({
-            url: 'tsm-form-fields.json',
-            dataType: 'json',
-            contentType: 'application/json',
-            success: function (data, textStatus, jqXHR) {
-                ajaxConsoleLog(textStatus, jqXHR);
-                tsmForm.buildForm(data);
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                ajaxConsoleLog(textStatus, jqXHR);
+    $.ajax({
+        //type: 'POST',
+        url: 'https://rtdev.cac.washington.edu/Tools/Offline.html',
+        //url : 'Offline.html',
+        //contentType: "application/x-www-form-urlencoded",
+        data: ticketStringPost,
+        //url: 'test.txt',
+        success: function(data, textStatus, jqXHR){
+            console.log('SUCCESS \n data = ' + data + ', textStatus = ' + textStatus);
+            ticket = Number(webForms.getNewTicketNumber(data));
+            //ticket = Number('random string');
+            if (isNaN(ticket) === true) {
+                $('#ticket-number').html('Something went awry');     
+            } else {
+                $('#ticket-number').html('Created RT Ticket <a href="' + rtDevURL + ticket + '">' + ticket + '</a>');
             }
-        });
-    };
-
-    tsmForm.buildForm = function (json) {
-        var html = '';
-        $.each(json, function(k, v){
-            console.log('k = ' + k + ', v = ' + v);
-            html += '<div class="control-group">' +
-                '<label class="control-label" for="input' + v.id + '">' + v.name + '</label>' +
-                '<div class="controls">' + 
-                '<input type="' + v.inputType +'" id="inputEmail" placeholder="' + v.placeholder + '">' + 
-                '</div>' +
-                '</div>';
-        });
-        $('.form-horizontal').append(html);
-    };
-
-    $(function () { 
-        tsmForm.displayForm();
+            //console.log('setQueue is ' + setQueue);
+            //console.log('user is ' + webForms.user);
+            //webForms.parcaeUser();
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('ERROR textStatus = ' + textStatus + ', errorThrown = ' + errorThrown);
+        }
     });
+}
 
-}());
+// http://stackoverflow.com/questions/905298/jquery-storing-ajax-response-into-global-variable
+webForms.getUser = function () { 
+    var url = 'user.php',
+        testUrl = 'user.json',
+        result = null;
+    $.ajax({
+        url: url,
+        async: false,
+        success: function(data) {
+            result = data;
+            //$('#uw-netid').text(result.user);
+        }
+    });
+    return result.user;
+};
+
+webForms.user = webForms.getUser();
+
+webForms.start = function (config) {
+    'use strict';
+    webForms.displayForm();
+    var uwNetID = webForms.getUser(),
+        queue = webForms.getQueue('ssgTest'),
+        subject = $('#subject').val(),
+        message = $('#message').val();
+    $('#uw-netid').text(uwNetID);
+    $('button').click(function() {
+        webForms.createTicket(subject, message, queue);
+    });
+};
+
+webForms.displayForm = function () {
+    console.log('running displayForm');
+    $.ajax({
+        url: 'test-form-config.json',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data, textStatus, jqXHR) {
+            ajaxConsoleLog('displayForm', textStatus, jqXHR);
+            webForms.buildForm(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            ajaxConsoleLog('displayForm', textStatus, jqXHR);
+        }
+    });
+};
+
+webForms.buildForm = function (json) {
+    var html = '';
+    $('h1').append(json.formName);
+    var input = ''; 
+    $.each(json.fields, function(k, v){
+        //console.log('k = ' + k + ', v = ' + v);
+        if (v.inputType === 'textarea') {
+            input = '<textarea rows="5"></textarea>';
+        } else {
+            input = '<input type="' + v.inputType +'" id="something" placeholder="' + v.placeholder + '">';
+        }
+        html += '<div class="control-group">' +
+            '<label class="control-label" for="input' + v.id + '">' + v.name + '</label>' +
+            '<div class="controls">' + 
+            input + '</div>' +
+            '</div>';
+    });
+    $('.form-horizontal').prepend(html);
+};
+
+$(function(){
+    'use strict';
+    //console.log('dom ready');
+    //webForms.parcaeUser();
+    webForms.start();
+});
