d91f0ea7d95089c2df80ff760c5064d0567ada91
jodytate <jtate@uw.edu>
2012-12-13 14:50:26 -0800

    config files, some setup after nebula meeting

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # UW-IT Web Forms
 
 <https://wiki.cac.washington.edu/x/Q8Q6Aw>
+
+Nebula/Exchange Form:
+https://www.washington.edu/itconnect/wares/nebula/forms/index.cgi
--- user.php
+++ user.php
@@ -1,14 +1,8 @@
 <?php 
-
     $authUser = $_SERVER[PHP_AUTH_USER];
     $userAgent = $_SERVER['HTTP_USER_AGENT'];
     $referer = $_SERVER['HTTP_REFERER'];
-
     $user = array('"user"' => $authUser, '"userAgent"' => $userAgent, '"referer"' => $referer);
-
     header('Content-type: application/json');
     echo json_encode($user);
-
 ?>
-
-
