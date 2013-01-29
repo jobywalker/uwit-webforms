30face186b2588d87df4dc19d10ae509efd8f427
jodytate <jtate@uw.edu>
2013-01-29 13:07:38 -0800

    nebula form is growing; clarifying tsm form based on notes

--- js/nebula.js
+++ js/nebula.js
@@ -1,5 +1,25 @@
 $(function () {
-        $('[name=authorizeRadios]').click(function(){ 
-            $('#authorizing-supervisor').fadeToggle();
-        });
+
+    $('[name=authorizeRadios]').click(function(){ 
+        $('#authorizing-supervisor').fadeToggle();
+    });
+
+    $('[name=accountActions]').click(function () {
+        accountAction = $(this, 'input').val();
+        console.log(accountAction)
+
+        if (accountAction === 'changePermissions') {
+            $('#change-permissions').fadeIn();
+        } else {
+            $('#change-permissions').fadeOut();
+        }
+        if (accountAction === 'changePermissions') {
+            $('#change-permissions').fadeIn();
+        } else {
+            $('#change-permissions').fadeOut();
+        }
+        
+
+    });
+
 });
